3aae349baaf80f02fb99f4ad7902e1ab2b6162f3
zaha <lelazahanara@gmail.com>
2020-11-26 02:04:07 +0600

    new(ui-update):making new login page
    making new login page

---
+++ layouts/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.primaryGradient {
+  /* background: linear-gradient(
+    to left,
+    rgba(26, 118, 224, 1) 0%,
+    rgba(32, 124, 229, 1) 100%
+  ); */
+  @apply bg-gradient-to-r;
+  @apply from-purple-400;
+  @apply via-pink-500;
+  @apply to-red-500;
+}
+</style>
