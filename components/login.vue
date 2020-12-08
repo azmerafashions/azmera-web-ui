08a87461da88f88c62064c8152d4e031f68e1927
zaha <lelazahanara@gmail.com>
2020-12-09 00:18:42 +0600

    login register(ui-update)
    login register

--- components/login.vue
+++ components/login.vue
@@ -1,57 +1,153 @@
 <template>
   <div class="w-full h-screen primaryGradient px-16 py-16">
-    <div class="bg-white rounded-xl shadow-lg min-h-full min-w-full p-10">
-      <h1 class="text-center mb-5 text-2xl text-pink-600">SIGN IN</h1>
-      <form action="">
-        <ul class="space-y-4 flex flex-col justify-between sp">
-          <li class="relative flex flex-row items-center input-animate">
-            <fa icon="user" class="z-10 absolute left-0 ml-2"> </fa>
-            <input
-              class="w-full input input-pink placeholder-black"
-              type="text"
-              placeholder="Username"
-            />
-          </li>
+    <div class="bg-white max-w-sm rounded-xl shadow-lg min-h-full p-10">
+      <div class="flex flex-col justify-center items-m align-middle">
+        <h1 class="text-center mb-5 text-2xl text-pink-600">SIGN IN</h1>
+        <form action="">
+          <ul class="space-y-4 flex flex-col justify-between sp">
+            <li class="relative flex flex-row items-center input-animate">
+              <fa icon="user" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black"
+                type="text"
+                placeholder="Username"
+              />
+            </li>
 
-          <li
-            class="relative flex flex-row items-center input-animate justify-start"
-          >
-            <fa icon="lock" class="z-10 absolute left-0 ml-2"> </fa>
-            <input
-              class="w-full input input-pink placeholder-black pl-12"
-              type="password"
-              placeholder="Password"
-            />
-          </li>
-          <li class="flex flex-row justify-between items-center">
-            <button
-              class="pink-button input-animate bg-pink-400 text-gray-800 w-1/2 text-lg button"
+            <li
+              class="relative flex flex-row items-center input-animate justify-start"
             >
-              submit
-            </button>
-            <nuxt-link
-              to=""
-              class="hover:text-black hover:bg-pink-400 px-2 py-1 rounded text-blue-700"
-            >
-              Forgot Password ?
-            </nuxt-link>
-          </li>
-          <li class="text-lg text-center text-pink-400 my-2">
-            ......... OR .........
-          </li>
-          <li class="flex flex-row justify-center items-center">
-            <button
-              class="relative pink-button input-animate bg-pink-400 text-gray-800 w-1/2 text-lg button"
-            >
-              Google
-              <fa icon="lock" class="z-10 absolute left-0 ml-2 my-1"> </fa>
-            </button>
-          </li>
-        </ul>
+              <fa icon="lock" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black pl-12"
+                type="password"
+                placeholder="Password"
+              />
+            </li>
+            <li class="flex flex-row justify-between items-center">
+              <button
+                class="pink-button input-animate bg-pink-400 text-gray-800 w-1/2 text-lg button"
+              >
+                submit
+              </button>
+              <nuxt-link
+                to=""
+                class="hover:text-black hover:bg-pink-400 px-2 py-1 rounded text-blue-700"
+                >Forgot Password?
+              </nuxt-link>
+            </li>
+            <li class="text-lg text-center text-pink-400 my-2">
+              ......... OR .........
+            </li>
+            <li class="flex flex-row justify-center items-center">
+              <button
+                class="relative pink-button input-animate bg-pink-400 text-gray-800 w-1/2 text-lg button"
+              >
+                Google
+                <fa icon="lock" class="z-10 absolute left-0 ml-2 my-1"> </fa>
+              </button>
+            </li>
+            <li>
+              <nuxt-link
+                class="text-center text-pink-400 w-full hover:text-pink-700"
+                to="/register"
+                >I dont have account? Register</nuxt-link
+              >
+            </li>
+          </ul>
 
-        <!-- <label for="password">Password</label>
+          <!-- <label for="password">Password</label>
         <input type="Password"/> -->
-      </form>
+        </form>
+      </div>
+    </div>
+
+    <!-- login register form -->
+    <div class="bg-white max-w-sm rounded-xl shadow-lg min-h-full p-10">
+      <div class="flex flex-col justify-center items-m align-middle">
+        <h1 class="text-center mb-5 text-2xl text-pink-600">Register</h1>
+        <form action="">
+          <ul class="space-y-4 flex flex-col justify-between sp">
+            <li class="relative flex flex-row items-center input-animate">
+              <fa icon="user" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black"
+                type="text"
+                placeholder="Fristname"
+              />
+            </li>
+            <li class="relative flex flex-row items-center input-animate">
+              <fa icon="user" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black"
+                type="text"
+                placeholder="Lastname"
+              />
+            </li>
+            <li class="relative flex flex-row items-center input-animate">
+              <fa icon="user" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black"
+                type="text"
+                placeholder="Email"
+              />
+            </li>
+            <li>
+              <span
+                ><label for="">Male</label>
+                <input type="radio" name="gender" id="" />
+                <label for="">Female</label>
+                <input type="radio" name="gender" id=""
+              /></span>
+            </li>
+            <li>
+              <label for="">Date of birth</label>
+              <input type="date" name="" id="" />
+            </li>
+            <li
+              class="relative flex flex-row items-center input-animate justify-start"
+            >
+              <fa icon="lock" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black pl-12"
+                type="password"
+                placeholder="Password"
+              />
+            </li>
+            <li
+              class="relative flex flex-row items-center input-animate justify-start"
+            >
+              <fa icon="lock" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black pl-12"
+                type="password"
+                placeholder="confirm Password"
+              />
+            </li>
+            <li class="flex flex-row justify-between items-center">
+              <button
+                class="pink-button input-animate bg-pink-400 text-gray-800 w-1/2 text-lg button"
+              >
+                submit
+              </button>
+            </li>
+            <li class="text-lg text-center text-pink-400 my-2">
+              ......... OR .........
+            </li>
+            <li class="flex flex-row justify-center items-center">
+              <button
+                class="relative pink-button input-animate bg-pink-400 text-gray-800 w-1/2 text-lg button"
+              >
+                Google
+                <fa icon="lock" class="z-10 absolute left-0 ml-2 my-1"> </fa>
+              </button>
+            </li>
+          </ul>
+
+          <!-- <label for="password">Password</label>
+        <input type="Password"/> -->
+        </form>
+      </div>
     </div>
     <!-- <div class="flex justify-center md:justify-between border-2">
       <div class="h-screen w-full p-10 flex flex-col md:flex-row">
---
+++ components/register.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="bg-white max-w-sm rounded-xl shadow-lg min-h-full p-10">
+      <div class="flex flex-col justify-center items-m align-middle">
+        <h1 class="text-center mb-5 text-2xl text-pink-600">Register</h1>
+        <form action="">
+          <ul class="space-y-4 flex flex-col justify-between sp">
+            <li class="relative flex flex-row items-center input-animate">
+              <fa icon="user" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black"
+                type="text"
+                placeholder="Fristname"
+              />
+            </li>
+            <li class="relative flex flex-row items-center input-animate">
+              <fa icon="user" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black"
+                type="text"
+                placeholder="Lastname"
+              />
+            </li>
+            <li class="relative flex flex-row items-center input-animate">
+              <fa icon="user" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black"
+                type="text"
+                placeholder="Email"
+              />
+            </li>
+            <li>
+              <span
+                ><label for="">Male</label>
+                <input type="radio" name="gender" id="" />
+                <label for="">Female</label>
+                <input type="radio" name="gender" id=""
+              /></span>
+            </li>
+            <li>
+              <label for="">Date of birth</label>
+              <input type="date" name="" id="" />
+            </li>
+            <li
+              class="relative flex flex-row items-center input-animate justify-start"
+            >
+              <fa icon="lock" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black pl-12"
+                type="password"
+                placeholder="Password"
+              />
+            </li>
+            <li
+              class="relative flex flex-row items-center input-animate justify-start"
+            >
+              <fa icon="lock" class="z-10 absolute left-0 ml-2"> </fa>
+              <input
+                class="w-full input input-pink placeholder-black pl-12"
+                type="password"
+                placeholder="confirm Password"
+              />
+            </li>
+            <li class="flex flex-row justify-between items-center">
+              <button
+                class="pink-button input-animate bg-pink-400 text-gray-800 w-1/2 text-lg button"
+              >
+                submit
+              </button>
+            </li>
+            <li class="text-lg text-center text-pink-400 my-2">
+              ......... OR .........
+            </li>
+            <li class="flex flex-row justify-center items-center">
+              <button
+                class="relative pink-button input-animate bg-pink-400 text-gray-800 w-1/2 text-lg button"
+              >
+                Google
+                <fa icon="lock" class="z-10 absolute left-0 ml-2 my-1"> </fa>
+              </button>
+            </li>
+          </ul>
+
+          <!-- <label for="password">Password</label>
+        <input type="Password"/> -->
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
